0cbf105b853313da7e042e5f1e3a8bb37df0779e
Abinaya-R20 <IT24102078@my.sliit.lk>
2024-10-19 11:35:21 +0530

    Add files via upload

---
+++ Q3/IT24102078Lab6Q3.java
@@ -0,0 +1,8 @@
+import.java.util.Scanner;
+public class IT24102078Lab6Q3{
+	public statics void main(strin[]args){
+		Scanner sc=new Scanner(System.in);
+		int sumOfSquares = 0;
+		int count = 0;
+		
+ System.out.println("Enter positive integers (terminate input with -99):")
